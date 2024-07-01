cad2032a20744d49ced6dd6867844238bfeb9c86
Aaron Harris <aaron.patrick.harris@gmail.com>
2024-07-01 11:14:52 -0500

    Configure Ktlint in `aph-kotlin` plugin
    Configure Ktlint as a linter again, but this time for downstream
    consumers of the `aph-kotlin` plugin.
    
    The version of Ktlint is currently hard-coded at 1.3.0.  This is the
    same version used in the plugin build itself, but because it needs to be
    specified in the plugin source itself, linking the two versions is
    challenging.  We can't reference the version catalog here because the
    catalog resolved will be that of the consuming project, not this one.
    The best long-term approach may be to leave the two references separate,
    but add a test to verify that they remain in sync, as discussed in
    [this blog post](https://lukeplant.me.uk/blog/posts/keeping-things-in-sync-derive-vs-test/).
    That will entail learning Gradle's plugin-testing tools, though, so
    let's leave that alone for now.
    
    More specific Ktlint configuration (such as disabled rules) is governed
    by the consuming project's `.editorconfig` file and is not specified
    here.
    
    Note that I am avoiding use of `spotless` as a type-safe accessor in the
    plugin source here, using `configure<SpotlessExtension>` instead.  This
    is because my IDE (IntelliJ 2024.1.4) can't recognize `spotless` without
    an explicit import of the generated accessor, which should not be
    necessary.  Gradle itself compiles the plugin fine, so I believe this is
    an IDE bug only, but better to avoid type-safe accessors until it is
    fixed.

--- aph-kotlin/src/main/kotlin/io.github.aaron-harris.gradle-conventions.aph-kotlin.gradle.kts
+++ aph-kotlin/src/main/kotlin/io.github.aaron-harris.gradle-conventions.aph-kotlin.gradle.kts
@@ -1,3 +1,22 @@
+import com.diffplug.gradle.spotless.BaseKotlinExtension
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
     id("kotlin")
+
+    id("com.diffplug.spotless")
+}
+
+configure<SpotlessExtension> {
+    val sharedKtlint: BaseKotlinExtension.() -> Unit = {
+        ktlint("1.3.0")
+            .setEditorConfigPath("$rootDir/.editorconfig")
+    }
+
+    kotlin {
+        sharedKtlint()
+    }
+    kotlinGradle {
+        sharedKtlint()
+    }
 }
